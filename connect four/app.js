30ac958dd7d1699f0bd10a841f5fdf01cf68c2e6
Kshama <210010025@iitdh.ac.in>
2023-01-12 21:27:46 +0530

    updated almost all files because kshama chutiya forgot to update every step

--- connect four/app.js
+++ connect four/app.js
@@ -1,52 +1,79 @@
-document.addEventListener('DOMContentLoaded',() =>{
+document.addEventListener('DOMContentLoaded', () => {
   const square = document.querySelectorAll('.grid div')
   const result = document.querySelector('#result')
-  const currplayer = document.querySelector('#currplayer')
+  const currentplayer = document.querySelector('#currentplayer')
 
   let cupl = 1
 
-  for(var i=0; i<square.length; i++){
+  for (var i = 0; i < square.length; i++) {
 
-  (function(index){
-    //add an onclick function to each square in your grid
-    square[index].onclick = function(){
-      if(square[index + 7].classList.contains('taken') ){
-        if(cupl===1){
-          square[index].classList.add('taken')
-          square[index].classList.add('plone')
-          cupl=2
-          currplayer.innerHTML=cupl
-        }
-        else{
-          square[index].classList.add('taken')
-          square[index].classList.add('pltwo')
-          cupl=1
-          currplayer.innerHTML=cupl
-        }
-      }
-      else alert ('Nooo!!!')
+    (function(index) {
+      //add an onclick function to each square in your grid
+      square[index].onclick = function() {
+        if (square[index + 7].classList.contains('taken')) {
+          if (cupl === 1) {
+            square[index].classList.add('taken')
+            square[index].classList.add('plone')
+            cupl = 2
+            currentplayer.innerHTML = cupl
+          } else {
+            square[index].classList.add('taken')
+            square[index].classList.add('pltwo')
+            cupl = 1
+            currentplayer.innerHTML = cupl
+          }
+        } else alert('Nooo!!!')
 
       }
-    })(i)
-}
+    })
 
-    function checkBoard(){
 
-      const winArrays = [
-        [0,1,2,3],
-        [1,2,3,4],
-        [2,3,4,5],
-        [3,4,5,6],
-        [7,8,9,10],
-        [8,9,10,11],
-        [,,,],
-        [,,,],
-        [,,,],
-        [,,,],
-        [,,,],
-        [,,,],
-        [,,,],
-      ]
+  function checkBoard() {
+
+    const winArrays = [
+      [0, 1, 2, 3],
+      [1, 2, 3, 4],
+      [2, 3, 4, 5],
+      [3, 4, 5, 6],
+      [7, 8, 9, 10],
+      [8, 9, 10, 11],
+      [9, 10, 11, 12],
+      [10, 11, 12, 13],
+      [14, 15, 16, 17],
+      [15, 16, 17, 18],
+      [16, 17, 18, 19],
+      [17, 18, 19, 20]
+    ]
+    for (let y = 0; y < winArrays.length; y++) {
+      const sq1 = square[winArrays[y][0]]
+      const sq2 = square[winArrays[y][1]]
+      const sq3 = square[winArrays[y][2]]
+      const sq4 = square[winArrays[y][3]]
+
+      if(sq1.classList.contains('plone') &&
+      sq2.classList.contains('plone')
+      &&sq3.classList.contains('plone') &&
+    sq4.classList.contains('plone') &&){
+      result.innerHTML = 'Player one wins!!'
+    }
+    else if (sq1.classList.contains('pltwo') &&
+    sq2.classList.contains('pltwo')
+    &&sq3.classList.contains('pltwo') &&
+  sq4.classList.contains('pltwo') &&) {
+    result.innerHTML = 'Player two wins!!'
+
+    }
     }
 
-  })
+  }(i)
+  //add an event listener to each square that will call the checkBoard function on click
+square.forEach(squu => squu.addEventListener('click',checkBoard))
+
+
+
+
+
+
+
+
+})
